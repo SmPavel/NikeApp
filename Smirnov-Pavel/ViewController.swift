3f65556dc76d0b922121edf2c5e7b2892abb1364
pavel <ass1954ass@yandex.ru>
2025-11-01 15:28:06 +0300

    Initial Commit

---
+++ Smirnov-Pavel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smirnov-Pavel
+//
+//  Created by CSF on 01.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
